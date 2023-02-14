183a005ec3ed2d2025ede87103f0aeaf37069db4
LukeDevsE <108841495+LukeDevsE@users.noreply.github.com>
2023-02-13 18:32:49 -0700

    Update content.js

--- content.js
+++ content.js
@@ -16,3 +16,32 @@ if (header) {
 }
 const bannerContainer = document.querySelector('.banner-container');
 bannerContainer.className = 'banner-container hidden';
+
+const navElements = ['nav-blog', 'nav-giftcards', 'nav-shop', 'nav-inventory'];
+
+navElements.forEach(id => {
+  const navElement = document.getElementById(id);
+  const parent = navElement.parentElement;
+
+  if (parent && parent.nodeName === 'LI') {
+    parent.remove();
+  } else {
+    navElement.remove();
+  }
+});
+
+const eventsElements = document.querySelectorAll('.font-bold.small.text-nav');
+
+eventsElements.forEach(element => {
+  if (element.innerText === 'Events' && element.nodeName === 'LI') {
+    element.remove();
+  }
+});
+
+const upgradeElements = document.querySelectorAll('.rbx-upgrade-now');
+
+upgradeElements.forEach(element => {
+  if (element.nodeName === 'LI') {
+    element.remove();
+  }
+});
